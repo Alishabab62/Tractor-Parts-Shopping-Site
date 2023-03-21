3e1d392cf7607d1615899907e26f9398d8c47d64
alishabab62 <alishabab62@gmail.com>
2023-03-21 22:43:59 +0530

    wishlist add

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Button(props) {
   return (
     <div>
-     <button onClick={props.fun}>{props.title}</button> 
+     <button onClick={props.fun} >{props.title}</button> 
     </div>
   )
 }
--- src/components/WishList.js
+++ src/components/WishList.js
@@ -1,14 +1,16 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import Header from './Header';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import axios from 'axios';
+
 export default function WishList() {
+  const [products , setProducts] = useState([])
   useEffect(()=>{
     let userData = JSON.parse(localStorage.getItem("userTractor"));
     const url = `https://tractor.onrender.com/wishlist/get/${userData.data.token}/${userData.data.email}`
     axios.get(url).then((res)=>{
-      console.log(res)
+      setProducts(res.data.data.product)
     }).catch((error)=>{
       console.log(error)
       if(error.response.status === 400){
@@ -17,10 +19,36 @@ export default function WishList() {
     })
   },[]) 
 
+  function handleRemoveWishlist(e){
+    const deleteItemId = e.target.getAttribute('item')
+    console.log(deleteItemId)
+  }
+
   return (
     <div>
       <Header/>
       <Navbar/>
+      <div className='main-wishlist-wrapper'>
+        <h3>Wishlist</h3>
+        <div className='wishlist-products-wrapper-main'>
+            {
+            products.map((item , index)=>{
+              return (
+              <div className='wishlist-product-wrapper' key={item._id}>
+              <img src={item.image} alt={"img"} onClick={handleRemoveWishlist}></img>
+              <div className='product-details'>
+                <div className='item-name'>{item.name}</div>
+                <div>{item.brand}</div>
+                <div>{item.price}</div>
+                  <button item={item._id}>Move to Kart</button>
+                  <button  item={item._id} onClick={handleRemoveWishlist}>Remove from WishList</button>
+              </div>
+            </div>
+              )
+            })
+          }
+        </div>
+      </div>
       <Footer/>
     </div>
   )
